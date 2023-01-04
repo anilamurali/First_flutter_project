4b56ff0e2c02c2e9ed0e74488cd761bf046d6b65
anilamurali <anilamurali98@gmail.com>
2023-01-04 22:31:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/myappss/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myappss
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+
+// import 'package:flutter/material.dart';
+// void main(){
+//     runApp(MaterialApp(home: SplashScreen()));
+    
+//   }
+  
+//   class SplashScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: Center(
+//         child: Icon(Icons.adb_outlined,
+//         size: 100,
+//         color: Colors.blue,),
+//       ),
+//       );
+//   }
+//   }
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    home: SplashScreen(),
+  ));
+
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Icon(
+          Icons.adb_outlined,
+          size: 100,
+          color: Colors.green,),
+      ),
+    );
+  }
+}
